dde6b2e531593d607b0d9be25d52d1cd3d243d6c
Durvankur4 <durvankurchavan4@gmail.com>
2026-01-30 21:54:48 +0530

    problem solved_94

---
+++ python_questions/leetcode/problem_94.py
@@ -0,0 +1,30 @@
+# # Quesiton:
+# 94. Binary Tree Inorder Traversal
+# Solved
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+# # Solution:
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         res = []
+#         def inorder(root):
+#             if not root :
+#                 return
+#             inorder(root.left)
+#             res.append(root.val)
+#             inorder(root.right)
+#         inorder(root)
+#         return res
+
+# # time complexity = O(n)
+# # space complexity = O(n)

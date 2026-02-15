775fa3c96575239430c9afa7462697fc6b3d1d35
Durvankur4 <durvankurchavan4@gmail.com>
2026-02-15 11:34:28 +0530

    problem solved_236

---
+++ python_questions/leetcode/problem_236.py
@@ -0,0 +1,42 @@
+# # Quesiton:
+# 236. Lowest Common Ancestor of a Binary Tree
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+# # Solution:
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.left = None
+# #         self.right = None
+
+# class Solution:
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+#         def dfs(node):
+#             if not node :
+#                 return
+
+#             if node == p or node == q:
+#                 return node
+
+#             left = dfs(node.left)
+#             right = dfs(node.right)
+
+#             if left and right :
+#                 return node
+
+#             return left if left else right
+
+#         return dfs(root)
+
+
+# # time complexity = O(n)
+# # space complexity = O(n)
